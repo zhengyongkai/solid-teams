60e1e6a56a44cfd0cef040786f0275d09809662c
zhengyongkai <825947557@qq.com>
2024-07-21 21:23:49 +0800

    ✨ feat(page):
    1. 添加 cursor:pointer

--- src/components/Common/Search/components/SearchItem.tsx
+++ src/components/Common/Search/components/SearchItem.tsx
@@ -3,7 +3,7 @@ import Avatar from '@/assets/img/avatar.png';
 export default function SearchItem() {
   return (
     <>
-      <div role="option" class="px-16 pt-7 pb-5 hover:bg-cnb1">
+      <div role="option" class="px-16 pt-7 pb-5 hover:bg-cnb1 cursor-pointer">
         <div class="flex items-center ">
           <div>
             <img src={Avatar} alt="" class="w-32 h-32 rounded-full" />
--- src/components/Common/Tag/Tag.tsx
+++ src/components/Common/Tag/Tag.tsx
@@ -1,14 +1,16 @@
-import { JSX } from 'solid-js'
+import { JSX } from 'solid-js';
 
 interface TagPropsInf {
-  active: boolean
-  children: JSX.Element
+  active: boolean;
+  children: JSX.Element;
 }
 
 export default function Tag(props: TagPropsInf) {
   return (
     <>
-      <span class="py-4 px-12 rounded-32 bg-cbb2 text-12">{props.children}</span>
+      <span class="py-4 px-12 rounded-32 bg-cbb2 text-12 cursor-pointer">
+        {props.children}
+      </span>
     </>
-  )
+  );
 }
