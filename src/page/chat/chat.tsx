dc945e1238160dbdd93e554585620075256124b2
zhengyongkai <825947557@qq.com>
2024-07-23 17:31:49 +0800

    ✨ feat(page):
    1. 提交

--- src/page/chat/chat.tsx
+++ src/page/chat/chat.tsx
@@ -1,15 +1,15 @@
-import ChatLeftBar from './components/ChatLeftBar';
-import ChatPanel from './components/ChatPanel';
+import ChatLeftBar from './components/ChatLeftBar'
+import ChatPanel from './components/ChatPanel'
 
 export default function chatPage() {
   return (
     <div class="flex h-full">
-      <div class="w-320 max-lg:hidden">
+      <div class="w-320 max-lg:hidden shadow-2xl">
         <ChatLeftBar />
       </div>
       <div class="flex-1">
         <ChatPanel />
       </div>
     </div>
-  );
+  )
 }
